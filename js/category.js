64518f42dccbd5691930ba36876dbf1d4165569a
mayuping <myp@qq.com>
2018-11-21 08:59:23 +0800

    分类页完成

--- js/category.js
+++ js/category.js
@@ -2,5 +2,30 @@
 
 // 一级分类动态渲染
 $(function(){
+  $.ajax({
+    type:"get",
+    url:"/category/queryTopCategory",
+    dataType:"json",
+    success:function(info){
+      // console.log(info);
+      $(".lt_main_left ul").html(template("firstTmp",info));
+      
+    }
+  })
 
+  // 二级分类动态渲染
+  $(".lt_main_left ul").on("click","a",function(){
+    var id = $(this).data("id");
+    $(this).addClass("current").parent().siblings().find("a").removeClass("current");
+    $.ajax({
+      type:"get",
+      url:"/category/querySecondCategory",
+      data:{id:id},
+      dataType:"json",
+      success:function(info){
+        // console.log(info);
+        $(".lt_main_right ul").html(template("secondTmp",info))
+      }
+    })
+  })
 })
